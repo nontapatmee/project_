5a4a68b461e3d427c1b480de867efe50f82b2083
nontapatmee <86467136+nontapatmee@users.noreply.github.com>
2022-04-16 15:59:23 +0700

    Add files via upload

--- code10_1.cpp
+++ code10_1.cpp
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string>
+#include <complex>
 
 using namespace std;
 
@@ -21,41 +22,43 @@ void initialize(double **u, double **u_new, double **v, double **v_new, double *
 
   for(int i=0; i<=nx-1; i++){
     for(int j=0; j<=ny-1; j++){
-      P[i][j] =	0;
-      P_old[i][j] = 0;
-      P_new[i][j] = 0;
+      P[i][j] =	1.;
+      P_old[i][j] = 1.;
+      P_new[i][j] = 1.;
             
     }
   }
   for(int i=0; i<=nx-1; i++){
     for(int j=0; j<=ny-2; j++){
-      G[i][j] = 0;
-      v[i][j] =	0;
-      v_new[i][j] = 0;
+      G[i][j] = 0.;
+      v[i][j] =	0.;
+      v_new[i][j] = 0.;
       
     }
   }
   for(int i=0; i<=nx-2; i++){
     for(int j=0; j<=ny-1; j++){
-      F[i][j] = 0;
-      u[i][j] = 0;
-      u_new[i][j] = 0;  
+      F[i][j] = 0.;
+      u[i][j] = 0.;
+      u_new[i][j] = 0.;  
     }
   }
   for(int i=0; i<=nx-2; i++){
     for(int j=0; j<=ny-2; j++){
-      Phi[i][j] = 0;
-      Phi_new[i][j] = 0;
+      Phi[i][j] = 0.;
+      Phi_new[i][j] = 0.;
       
       }
   }
   
   for(int j=1; j<=ny-2; j++){
-    u[0][j] = 1;  
+    u[0][j] = 1.;
   }
   for(int j=1; j<=((ny-3)/2); j++){
-    Phi[0][j] = 1;  
+    Phi[0][j] = 1.;  
   }
+
+  
   
 }
 
@@ -87,7 +90,7 @@ void simulation_FG(double **u, double **u_new, double **v, double **v_new, doubl
   
   for(int i=1; i<=nx-3; i++){
     for(int j =1; j<=ny-2; j++){
-        RHS_F1 = (((u[i+1][j]+2*u[i][j]+u[i-1][j])/pow(dx,2.))+((u[i][j+1]+2*u[i][j]+u[i][j-1])/pow(dy,2.)))/Re;
+        RHS_F1 = (((u[i+1][j]-2*u[i][j]+u[i-1][j])/pow(dx,2.))+((u[i][j+1]-2*u[i][j]+u[i][j-1])/pow(dy,2.)))/Re;
 	//S_F1 = (((u[i][j+1]+2*u[i][j]+u[i][j-1])/pow(dy,2.)))/Re;
       
       RHS_F2 = (pow(((u[i][j]+u[i+1][j])/2),2.)-pow(((u[i-1][j]+u[i][j])/2),2.))/dx;
@@ -97,17 +100,22 @@ void simulation_FG(double **u, double **u_new, double **v, double **v_new, doubl
     }
   }
   for(int j =1; j<=ny-2; j++){
-   F[0][j]=1;
+    F[0][j]=0;
+    F[nx-2][j]=F[nx-3][j];
   }
 
-
-  for(int j=1; j<=ny-2; j++){
-  F[nx-2][j]=F[nx-3][j];
+  //bd no-slip
+  for(int i=1; i<=nx-3; i++){
+    F[i][0]=(-1)*F[i][1];
+    F[i][ny-1]=(-1)*F[i][ny-2];
   }
+  //visualize(F,nx-1,ny);
+
+  
   //////////////////////
   for(int i=1; i<=nx-2; i++){
     for(int j =1; j<=ny-3; j++){
-      RHS_G1 = (((v[i+1][j]+2*v[i][j]+v[i-1][j])/pow(dx,2.))+((v[i][j+1]+2*v[i][j]+v[i][j-1])/pow(dy,2.)))/Re;
+      RHS_G1 = (((v[i+1][j]-2*v[i][j]+v[i-1][j])/pow(dx,2.))+((v[i][j+1]-2*v[i][j]+v[i][j-1])/pow(dy,2.)))/Re;
       //RHS_G1 = (((v[i+1][j]+2*v[i][j]+v[i-1][j])/pow(dx,2.)))/Re;
 
       RHS_G3 = (pow(((v[i][j]+v[i][j+1])/2),2.)-pow(((v[i][j-1]+v[i][j])/2),2.))/dy;
@@ -119,9 +127,10 @@ void simulation_FG(double **u, double **u_new, double **v, double **v_new, doubl
   }
 
   for(int j = 1; j<=ny-3; j++){
+    G[0][j]=(-1)*G[1][j];
     G[nx-1][j]=G[nx-2][j];
   }
-
+ 
 
   //boundary Neumann
   //for(int i=1; i<=nx-2; i++){
@@ -134,42 +143,63 @@ void simulation_p(double **u, double **u_new, double **v, double **v_new, double
   double aa, bb, cc;
 
   double ee, ew, en, es;
-
+  double RHS;
   bool status = true;
-  aa = 1/pow(dx,2.);
-  bb = 1/pow(dy,2.);
+  aa = 1./pow(dx,2.);
+  bb = 1./pow(dy,2.);
   cc = (pow(dx,2.)*pow(dy,2.))/(2*(pow(dx,2.)+pow(dy,2.)));
   //  cc = (pow(dx,2.)*pow(dy,2.))/((en+es)*pow(dx,2.)+(ee+ew)*pow(dy,2.));
 
-  double w=1.7;
-  
+  double w=1.;
+
+  //update bd condition F(0,j) <= u(0,j) , v => G(i,0)
+  for(int j=1; j<=ny-2; j++){
+     F[0][j]=u[0][j];
+     F[nx-2][j]=u[nx-2][j];
+   }
+   for(int i=1; i<=ny-2; i++){
+     G[i][0]=v[i][0];
+     G[i][nx-2]=v[i][nx-2];
+   }
+   //visualize(F,nx-1,ny);
+  //visualize(G,ny,ny-1);
   while(true){
     //for(int c=1; c<100; c++){
     if(status == false){break;}
 
     for(int i=0; i<=nx-1; i++){
       for(int j =0; j<=ny-1; j++){
-	P[i][j]=P_new[i][j];
+    	P[i][j]=P_new[i][j];
       }
     }
 
-	
+    // for(int i=1; i<=nx-2; i++){
+    //   for(int j =1; j<=ny-2; j++){
+    // 	P[i][j]=P_new[i][j];
+    //   }
+    // }
+
+
+ 
+    
     for(int i=1; i<=nx-2; i++){
       for(int j =1; j<=ny-2; j++){
 	//P_new[i][j]=((((F[i][j]-F[i-1][j])/dx+(G[i][j]-G[i][j-1])/dy)*(-1/dt))+(aa*P[i+1][j]+aa*P_new[i-1][j]+bb*P[i][j+1]+bb*P_new[i][j-1]))*cc;
 
-	if(i==1){ew=0;}
+	if(i==1){ew=1;}
         else{ew=1;}
-        if(i==nx-2){ee=0;}
+        if(i==nx-2){ee=1;}
         else{ee=1;}
-        if(j==1){es=0;}
+        if(j==1){es=1;}
         else{es=1;}
         if(j==ny-2){en=1;}
         else{en=1;}
 
         cc = (pow(dx,2.)*pow(dy,2.))/((en+es)*pow(dx,2.)+(ee+ew)*pow(dy,2.));
         //cout<<"   "<<cc<<"\n";
-        P_new[i][j]=(  ( (((F[i][j]-F[i-1][j])/dx) + ((G[i][j]-G[i][j-1])/dy))*(-1/dt) )  +  ( aa*ee*P[i+1][j] + aa*ew*P_new[i-1][j] + bb*en*P[i][j+1] + bb*es*P_new[i][j-1] )  )*cc;
+        P_new[i][j]=(  ( (((F[i][j]-F[i-1][j])/dx) + ((G[i][j]-G[i][j-1])/dy))*(-1./dt) )  +  ( aa*ee*P[i+1][j] + aa*ew*P_new[i-1][j] + bb*en*P[i][j+1] + bb*es*P_new[i][j-1] )  )*cc;
+	//RHS=((F[i][j]-F[i-1][j])/dx + (G[i][j]-G[i][j-1])/dy)/dt;
+        //P_new[i][j]=(1-w)*P[i][j] +  (w/((ee+ew)/pow(dx,2) + (en+es)/pow(dy,2)))*((ee*P[i+1][j]+ew*P_new[i-1][j])/pow(dx,2) + (en*P[i][j+1]+es*P_new[i][j-1])/pow(dy,2) - RHS);
 	
 	status = false;
       }
@@ -191,7 +221,7 @@ void simulation_p(double **u, double **u_new, double **v, double **v_new, double
   
     for(int j=1; j<=ny-2; j++){
       P_new[0][j] = P_new[1][j];
-      P_new[nx-1][j] = P_new[nx-2][j];
+      //P_new[nx-1][j] = P_new[nx-2][j];
     }
     for(int i=1; i<=nx-2; i++){
       P_new[i][0] = P_new[i][1];
@@ -236,8 +266,14 @@ void simulation_uv(double **u, double **u_new, double **v, double **v_new, doubl
    u_new[nx-2][j]=u_new[nx-3][j];
  }
  for(int j=1; j<=ny-3; j++){
+   v_new[0][j]=(-1)*v_new[1][j];
    v_new[nx-1][j]=v_new[nx-2][j];
  }
+
+ for(int i=1; i<=nx-3; i++){
+    u_new[i][0]=(-1)*u_new[i][1];
+    u_new[i][ny-1]=(-1)*u_new[i][ny-2];
+  }
  
 }
 
@@ -304,12 +340,12 @@ void paraview(string fileName, double **var, int nx, int ny, double dx, double d
 
 int main(){
 
-  int nx = 10;
-  int ny = 10;
-  double Re = 170.;
-  double dx = 1;
-  double dy = 1;
-  double dt = 0.005;
+  int nx = 40;
+  int ny = 20;
+  double Re = 200.;
+  double dx = 1./real(nx-2);
+  double dy = 1./real(ny-2);
+  double dt = 0.001;
   string fileName;
   
   double **u;
@@ -375,7 +411,7 @@ int main(){
   //visualize(v,nx,ny);
   
   initialize(u,u_new,v,v_new,F,G,P,P_old,P_new,Phi,Phi_new,nx,ny);
-for (int n = 1; n<=3; n++){  
+for (int n = 1; n<=300; n++){  
   simulation_FG(u, u_new, v, v_new, F, G, nx, ny, Re, dx, dy, dt);
   simulation_p(u, u_new, v, v_new, F, G, P, P_new, nx, ny, Re, dx, dy, dt);
   simulation_uv(u, u_new, v, v_new, F, G, P, P_new, nx, ny, Re, dx, dy, dt);
@@ -387,7 +423,7 @@ for (int n = 1; n<=3; n++){
   
   cout << "n = " << n << "\n";
  
-  if( n%1 == 0) {
+  if( n%2 == 0) {
   fileName = "phi_" + to_string(n) + ".vtk";
   paraview(fileName, Phi, nx-1, ny-1, dx, dy);
   fileName = "u_" + to_string(n) + ".vtk";
@@ -395,11 +431,11 @@ for (int n = 1; n<=3; n++){
   fileName = "p_" + to_string(n) + ".vtk";
   paraview(fileName, P, nx, ny, dx, dy);
   
-  //visualize(F,nx-1,ny);
+  // visualize(F,nx-1,ny);
   //visualize(u_new,nx-1,ny);
   // visualize(G,nx,ny-1);
-  // visualize(v,nx,ny-1);
-  visualize(P_new,nx,ny);
+  //visualize(v,nx,ny-1);
+  //visualize(P_new,nx,ny);
   //visualize(Phi_new,nx-1,ny-1);
   }
 }
